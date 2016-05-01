1aef6c2b9253f9032e252aa1e7e33127aea9b4c4
Home <Home@ISS_STF-KBALINT>
2016-05-01 20:35:30 +0200

    Add project files.

---
+++ WAF_Bead_bg5q8g/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WAF_Bead_bg5q8g.MvcApplication" Language="C#" %>
---
+++ WAF_Bead_bg5q8g/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WAF_Bead_bg5q8g.Startup))]
+namespace WAF_Bead_bg5q8g
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
