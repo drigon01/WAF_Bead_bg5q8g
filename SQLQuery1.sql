6e67d675d62f7e07bc304215b741c55c96b17421
Home <Home@ISS_STF-KBALINT>
2016-05-03 22:31:34 +0200

    getting there

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -2,7 +2,7 @@
 
 INSERT INTO News_Portal.dbo.Articles VALUES(NEWID(),'EXtronauts released',GETDATE(),'New boardgame designed by NASA engineers has been productized.','blahblahblahblahblahblahblahblahblahblahblah',1,'814334d0-c28e-43e2-83b1-404ffb01d03f');
 INSERT INTO News_Portal.dbo.Articles VALUES(NEWID(),'SaltCity', GETDATE(),'New boardgame SaltCity is marching toward its kikstarter campaing','blahblahblahblahblahblahblahblahblahblahblah',1,'814334d0-c28e-43e2-83b1-404ffb01d03f');
-INSERT INTO News_Portal.dbo.Articles VALUES(NEWID(),'Aye my dark overlord', GETDATE(),'New chapter has been released to the well known game.','blahblahblahblahblahblahblahblahblahblahblah',1,'814334d0-c28e-43e2-83b1-404ffb01d03f');
+INSERT INTO News_Portal.dbo.Articles VALUES('70555247-1d57-4498-9f93-2ca3661a5f44','Aye my dark overlord', GETDATE(),'New chapter has been released to the well known game.','blahblahblahblahblahblahblahblahblahblahblah',1,'814334d0-c28e-43e2-83b1-404ffb01d03f');
 INSERT INTO News_Portal.dbo.Articles VALUES(NEWID(),'solo', GETDATE(),'No more solo/uno/etc. are alowed in sokovia.','blahblahblahblahblahblahblahblahblahblahblah',1,'814334d0-c28e-43e2-83b1-404ffb01d03f');
 INSERT INTO News_Portal.dbo.Articles VALUES(NEWID(),'waaaaaaaaa',GETDATE(),'New boardgame designed by NASA engineers has been productized.','blahblahblahblahblahblahblahblahblahblahblah',1,'814334d0-c28e-43e2-83b1-404ffb01d03f');
 INSERT INTO News_Portal.dbo.Articles VALUES(NEWID(),'vittu', GETDATE(),'New boardgame SaltCity is marching toward its kikstarter campaing','blahblahblahblahblahblahblahblahblahblahblah',1,'814334d0-c28e-43e2-83b1-404ffb01d03f');
@@ -18,3 +18,10 @@ INSERT INTO News_Portal.dbo.Articles VALUES(NEWID(),'dominion',GETDATE(),'New bo
 INSERT INTO News_Portal.dbo.Articles VALUES(NEWID(),'carcassone', GETDATE(),'New boardgame SaltCity is marching toward its kikstarter campaing','blahblahblahblahblahblahblahblahblahblahblah',1,'814334d0-c28e-43e2-83b1-404ffb01d03f');
 INSERT INTO News_Portal.dbo.Articles VALUES(NEWID(),'citadella', GETDATE(),'New chapter has been released to the well known game.','blahblahblahblahblahblahblahblahblahblahblah',1,'814334d0-c28e-43e2-83b1-404ffb01d03f');
 INSERT INTO News_Portal.dbo.Articles VALUES(NEWID(),'roadkill rally', GETDATE(),'No more solo/uno/etc. are alowed in sokovia.','blahblahblahblahblahblahblahblahblahblahblah',1,'814334d0-c28e-43e2-83b1-404ffb01d03f');
+
+INSERT INTO News_Portal.dbo.Images(Id,News_Id,Article_Id,Image1)
+SELECT 'f2172f01-0dc4-427f-91aa-74e66b06b4b4','70555247-1d57-4498-9f93-2ca3661a5f44','70555247-1d57-4498-9f93-2ca3661a5f44',BulkColumn 
+FROM Openrowset( Bulk 'D:\shared\waf\WAF_Bead_bg5q8g\WAF_Bead_bg5q8g\Content\aye.jpg', Single_Blob) as Image
+INSERT INTO News_Portal.dbo.Images(Id,News_Id,Article_Id,Image1)
+SELECT 'f6172f01-0dc4-427f-91aa-74e66b06b4b4','70555247-1d57-4498-9f93-2ca3661a5f44','70555247-1d57-4498-9f93-2ca3661a5f44',BulkColumn 
+FROM Openrowset( Bulk 'D:\shared\waf\WAF_Bead_bg5q8g\WAF_Bead_bg5q8g\Content\aye.jpg', Single_Blob) as Image
--- WAF_Bead_bg5q8g/Controllers/HomeController.cs
+++ WAF_Bead_bg5q8g/Controllers/HomeController.cs
@@ -46,105 +46,16 @@ namespace WAF_Bead_bg5q8g.Controllers
     /// <summary>
     /// Query image for article.
     /// </summary>
-    /// <param name="articleId">article id</param>
+    /// <param name="Id">article id</param>
     /// <returns>the image associated with the article, or a default one</returns>
-    public FileResult ArticelImage(Guid articleId)
+    public FileResult ArticleImage(Guid Id)
     {
-      if (articleId == null) { return File("~/Content/missing.jpg", "image/jpg"); }
-      Byte[] wImageContent = mEntities.Images.Where(image => image.Id == articleId).Select(image => image.Image1).FirstOrDefault();
+      if (Id == null) { return File("~/Content/missing.jpg", "image/jpg"); }
+      Byte[] wImageContent = mEntities.Images.Where(image => image.Id == Id).Select(image => image.Image1).FirstOrDefault();
 
       if (wImageContent == null) { return File("~/Content/missing.jpg", "image/jpg"); }
 
-      return File(wImageContent, "image/png");
+      return File(wImageContent, "image/jpg");
     }
-
-    /// <summary>
-    /// Épület egyik képének lekérdezése.
-    /// </summary>
-    /// <param name="imageId">Kép azonosítója.</param>
-    /// <param name="isLarge">Nagy méretű kép lekérése.</param>
-    /// <returns>Az épület egy képe, vagy az alapértelmezett kép.</returns>
-    public FileResult Image(Guid? imageId, Boolean isLarge = false)
-    {
-      if (imageId == null) // nem adtak meg azonosítót
-        return File("~/Content/missing.jpg", "image/jpg");
-
-      // lekérjük a megadott azonosítóval rendelkező képet
-      byte[] imageContent = mEntities.Images.Where(image => image.Id == imageId).Select(image => isLarge ? image.Image1 : CreateThumbnail(image.Image1, 100)).FirstOrDefault();
-
-      if (imageContent == null) // amennyiben nem sikerült betölteni, egy alapértelmezett képet adunk vissza
-        return File("~/Content/missing.jpg", "image/jpg");
-
-      return File(imageContent, "image/jpg");
-    }
-
-
-    //Referenced from : https://social.msdn.microsoft.com/Forums/vstudio/en-US/e2d8bd99-1af4-404a-a6bd-5fae9f540c2d/how-to-resize-an-image-in-byte-?forum=csharpgeneral
-    // (RESIZE an image in a byte[] variable.)   should move to separatae class
-    #region image manipulation 
-    private static byte[] CreateThumbnail(byte[] PassedImage, int LargestSide)
-    {
-      byte[] ReturnedThumbnail;
-
-      using (MemoryStream StartMemoryStream = new MemoryStream(),
-                          NewMemoryStream = new MemoryStream())
-      {
-        // write the string to the stream  
-        StartMemoryStream.Write(PassedImage, 0, PassedImage.Length);
-
-        // create the start Bitmap from the MemoryStream that contains the image  
-        Bitmap startBitmap = new Bitmap(StartMemoryStream);
-
-        // set thumbnail height and width proportional to the original image.  
-        int newHeight;
-        int newWidth;
-        double HW_ratio;
-        if (startBitmap.Height > startBitmap.Width)
-        {
-          newHeight = LargestSide;
-          HW_ratio = (double)((double)LargestSide / (double)startBitmap.Height);
-          newWidth = (int)(HW_ratio * (double)startBitmap.Width);
-        }
-        else
-        {
-          newWidth = LargestSide;
-          HW_ratio = (double)((double)LargestSide / (double)startBitmap.Width);
-          newHeight = (int)(HW_ratio * (double)startBitmap.Height);
-        }
-
-        // create a new Bitmap with dimensions for the thumbnail.  
-        Bitmap newBitmap = new Bitmap(newWidth, newHeight);
-
-        // Copy the image from the START Bitmap into the NEW Bitmap.  
-        // This will create a thumnail size of the same image.  
-        newBitmap = ResizeImage(startBitmap, newWidth, newHeight);
-
-        // Save this image to the specified stream in the specified format.  
-        newBitmap.Save(NewMemoryStream, System.Drawing.Imaging.ImageFormat.Jpeg);
-
-        // Fill the byte[] for the thumbnail from the new MemoryStream.  
-        ReturnedThumbnail = NewMemoryStream.ToArray();
-      }
-
-      // return the resized image as a string of bytes.  
-      return ReturnedThumbnail;
-    }
-
-    // Resize a Bitmap  
-    private static Bitmap ResizeImage(Bitmap image, int width, int height)
-    {
-      Bitmap resizedImage = new Bitmap(width, height);
-      using (Graphics gfx = Graphics.FromImage(resizedImage))
-      {
-        gfx.DrawImage(image, new Rectangle(0, 0, width, height),
-            new Rectangle(0, 0, image.Width, image.Height), GraphicsUnit.Pixel);
-      }
-      return resizedImage;
-    }
-
-    #endregion
-
-
-
   }
 }
